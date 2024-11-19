38c6ba314452b682675bea466d16792d3efe553d
Ryohei Fujihara <giver.yell@gmail.com>
2024-11-19 10:58:59 +0900

    Run two block number

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 
-const executeQuery = async (url, query) => {
+const executeQuery = async (url, query, blockNumber) => {
   try {
     const response = await fetch(url, {
       method: 'POST',
@@ -12,26 +12,31 @@ const executeQuery = async (url, query) => {
 
     const resultText = await response.text();
     if (!response.ok) {
-      console.error('GraphQL Error Response:', resultText);
+      console.error(`GraphQL Error Response (Block ${blockNumber}):`, resultText);
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
     const result = JSON.parse(resultText);
-    console.log('Query Result:', JSON.stringify(result, null, 2));
+    console.log(`Query Result for Block ${blockNumber}:`, JSON.stringify(result, null, 2));
   } catch (error) {
-    console.error('Error executing query:', error);
+    console.error(`Error executing query for Block ${blockNumber}:`, error);
   }
 };
 
 const url = 'https://graph.mainnet.oasys.games/subgraphs/name/oasys/staking';
 
-const query = `
+const createQuery = (blockNumber) => `
 {
-  validators(orderBy: id, first: 1000, block: { number: 5095899 }, where: { id: "0x4e5e774d3837bd9302b83cad94a112575411f07b" }) {
+  validators(orderBy: id, first: 1000, block: { number: ${blockNumber} }, where: { id: "0x4e5e774d3837bd9302b83cad94a112575411f07b" }) {
     id
     commissions
   }
 }
 `;
 
-executeQuery(url, query);
+// execute
+const blockNumbers = [5095900, 5095898];
+blockNumbers.forEach((blockNumber) => {
+  const query = createQuery(blockNumber);
+  executeQuery(url, query, blockNumber);
+});
